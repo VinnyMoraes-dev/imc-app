df1f3ce1cf230f0f18345ed9ddb75ba6ec1fb21f
Vinny Moraes <56524332+VinnyMoraes-dev@users.noreply.github.com>
2022-06-06 21:34:37 -0300

    Add files via upload
    Não adicionou o node_modules pois contém mais de 100 arquivos e é superior a 25 MB.

---
+++ src/App.vue
@@ -0,0 +1,105 @@
+<template>
+  <h1 class="label-title">Cáculo do IMC</h1>
+  <h2 class="label-subtitle">Digite seu peso e altura para calcular o IMC</h2>
+  
+  <div class="div-imc">
+    <span class="p-float-label">
+      <InputText id="input-weight" type="text" v-model="weight" v-bind:disabled="imc"/>
+      <label class="label2" for="input-weight">Peso</label>  
+    </span>
+  </div>
+
+  <div class="div-imc">
+    <span class="p-float-label">
+      <InputText id="input-height" type="text" v-model="height" v-bind:disabled="imc"/>
+      <label class="label2" for="input-height">Altura</label>  
+    </span>
+  </div>
+
+  <div class="div-imc" v-if="!imc">
+    <button-item label="Limpar" @click="clear"/>
+    <button-item label="Calcular" @click="calculate" class="button-calculate"/>
+  </div>
+
+  <div v-if="imc">
+    <p class="label-result">Seu IMC é: {{ imc }}</p>
+    <p class="label-classification">A classificação do seu IMC é: {{ classification }}</p>
+    <button-item label="Calcular Novamente" @click="clear"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      height: null,
+      weight: null,
+      imc: null,
+      classification: ""
+    }
+  },
+
+  methods: {
+    calculate: function() {
+      this.imc = (this.weight / (this.height * this.height)).toFixed(2);
+      if (this.imc < 18.5) {
+        this.classification = "Magreza";
+      } else if (this.imc >= 18.5 && this.imc < 25) {
+        this.classification ="Normal";
+      } else if (this.imc >= 25 && this.imc < 30) {
+        this.classification ="Sobrepeso";
+      } else if (this.imc >= 30 && this.imc < 40) {
+        this.classification ="Obesidade";
+      } else {
+        this.classification ="Obesidade Grave";
+      }
+    },
+
+    clear() {
+      this.height = null;
+      this.weight = null;
+      this.imc = null;
+      this.result = "";
+      this.classification = "";
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.div-imc {
+  margin-top: 1rem;
+}
+
+.button-calculate {
+  margin-left: 1rem!important;
+}
+
+.label-title {
+  font-size: 2rem;
+}
+
+.label-subtitle {
+  font-size: 1.1rem;
+}
+
+.label2 {
+  left: 31.5rem!important;
+}
+
+.label-result {
+  font-size: 2rem;
+}
+
+.label-classification {
+  font-size: 1.5rem;
+}
+
+</style>
